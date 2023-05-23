082de89c7e5df87f07e3f5ae25e9a1337277a318
호야 <107461356+hoya0319@users.noreply.github.com>
2023-05-23 19:46:25 +0900

    Update typhoonmap.js

--- source/javascript/gongong/data/api/jsfiles/typhoonmap.js
+++ source/javascript/gongong/data/api/jsfiles/typhoonmap.js
@@ -50,12 +50,282 @@ xhr.onreadystatechange = function () {
         }
         console.log(time)
         console.log(num)
-        console.log(time[0])
-        var url = 'https://apis.data.go.kr/1360000/TyphoonInfoService/getTyphoonInfo'; /*URL*/
-        var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'cl5s8i4yp76CKdNIDbn0RZDOYdzjAgzPaTtbVMDqnKWHomjjBtq%2BmajQpYggkXVlfj4FY2x304%2FuTVIm1DilIw%3D%3D'; /*Service Key*/
-        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /*페이지 번호 조회*/
-        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('30'); /*행 수*/
-        queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('JSON'); /*데이터 타입 - JSON*/
+
+        
+        var info_xhr = new XMLHttpRequest();
+        var info_url = 'https://apis.data.go.kr/1360000/TyphoonInfoService/getTyphoonFcst'; /*URL*/
+        var info_queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'cl5s8i4yp76CKdNIDbn0RZDOYdzjAgzPaTtbVMDqnKWHomjjBtq%2BmajQpYggkXVlfj4FY2x304%2FuTVIm1DilIw%3D%3D'; /*Service Key*/
+        info_queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /*데이터 타입 - JSON*/
+        info_queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /*데이터 타입 - JSON*/
+        info_queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('JSON'); /*데이터 타입 - JSON*/
+        info_queryParams += '&' + encodeURIComponent('tmFc') + '=' + encodeURIComponent(time[0]); /*조회 끝 날짜 - 오늘*/
+        info_queryParams += '&' + encodeURIComponent('typSeq') + '=' + encodeURIComponent(num[0]); /*조회 끝 날짜 - 오늘*/
+        info_xhr.open('GET', info_url + info_queryParams);
+        // console.log(info_url + info_queryParams)
+        info_xhr.open('GET', info_url + info_queryParams);
+        info_xhr.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                try {
+                    var jsonData = JSON.parse(this.responseText);
+                } catch (error) {
+
+                }
+                // console.log(jsonData.response.body.items.item)
+                var asdf = jsonData.response.body.items.item;
+                lat=[]
+                lat.push(asdf[0].lat)
+                lat.push(asdf[0].lon)
+                plz.push(lat)
+                for (j = 0; j < asdf.length; j++) {
+                    var latlondata = []
+                    latlondata.push(asdf[j].lat)
+                    latlondata.push(asdf[j].lon)
+                    latlon.push(latlondata)
+                }
+                // console.log(latlon)
+                for (j = 0; j < latlon.length; j++) {
+                    L.marker(latlon[j], { icon: ping }).addTo(map)
+                }
+                //현재
+                if (asdf[0].ws >= 54) {
+                    strength = "초강력"
+                } else if (asdf[0].ws >= 45) {
+                    strength = "매우강"
+                } else if (asdf[0].ws >= 33) {
+                    strength = "강"
+                } else if (asdf[0].ws >= 25) {
+                    strength = "중"
+                } else {
+                    strength = "--"
+                }
+                for (k = latlon.length - 1; k >= 0; k--) {
+                    L.circle(latlon[k], {
+                        color: 'white',
+                        fillColor: 'rgba(255, 0, 0, 0)',
+                        radius: (asdf[k].radPr) * 1000,
+                        dashArray: '10, 10',
+                        dashOffset: '0',
+                        weight: 2
+                    }).addTo(map)
+                }
+                function getStrength(wind){
+                    if (wind >= 54) {
+                        strength = "초강력"
+                    } else if (wind >= 45) {
+                        strength = "매우강"
+                    } else if (wind >= 33) {
+                        strength = "강"
+                    } else if (wind >= 25) {
+                        strength = "중"
+                    } else {
+                        strength = "--"
+                    }
+                    return strength
+                }
+                rad=[]
+                for(j=0;j<asdf.length;j++){
+                    rad.push(Number(asdf[j].radPr) + Number(asdf[j].rad25))
+                }
+                // console.log(rad)
+                try{
+                    if(asdf[6].rad25== 0){
+                        var d7 = L.circle(latlon[6],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[6]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d7 = L.circle(latlon[6],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[6]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d7.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[6].tm).slice(6,8)}일 ${(asdf[6].tm).slice(8,10)}시 ${(asdf[6].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[6].ps}hPa<br>최대풍속: ${asdf[6].ws}m/s<br>강도: ${getStrength(asdf[6].ws)}<br>진행: ${asdf[6].dir}, ${asdf[6].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d7.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+                try{
+                    if(asdf[5].rad25== 0){
+                        var d6 = L.circle(latlon[5],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[5]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d6 = L.circle(latlon[5],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[5]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d6.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[5].tm).slice(6,8)}일 ${(asdf[5].tm).slice(8,10)}시 ${(asdf[5].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[5].ps}hPa<br>최대풍속: ${asdf[5].ws}m/s<br>강도: ${getStrength(asdf[5].ws)}<br>진행: ${asdf[5].dir}, ${asdf[5].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d6.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+                try{
+                    if(asdf[4].rad25== 0){
+                        var d5 = L.circle(latlon[4],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[4]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d5 = L.circle(latlon[4],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[4]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d5.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[4].tm).slice(6,8)}일 ${(asdf[4].tm).slice(8,10)}시 ${(asdf[4].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[4].ps}hPa<br>최대풍속: ${asdf[4].ws}m/s<br>강도: ${getStrength(asdf[4].ws)}<br>진행: ${asdf[4].dir}, ${asdf[4].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d5.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+                try{
+                    if(asdf[3].rad25== 0){
+                        var d4 = L.circle(latlon[3],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[3]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d4 = L.circle(latlon[3],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[3]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d4.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[3].tm).slice(6,8)}일 ${(asdf[3].tm).slice(8,10)}시 ${(asdf[3].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[3].ps}hPa<br>최대풍속: ${asdf[3].ws}m/s<br>강도: ${getStrength(asdf[3].ws)}<br>진행: ${asdf[3].dir}, ${asdf[3].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d4.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+                try{
+                    if(asdf[2].rad25== 0){
+                        var d3 = L.circle(latlon[2],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[2]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d3 = L.circle(latlon[2],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[2]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d3.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[2].tm).slice(6,8)}일 ${(asdf[2].tm).slice(8,10)}시 ${(asdf[2].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[2].ps}hPa<br>최대풍속: ${asdf[2].ws}m/s<br>강도: ${getStrength(asdf[2].ws)}<br>진행: ${asdf[2].dir}, ${asdf[2].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d3.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+                try{
+                    if(asdf[1].rad25== 0){
+                        var d2 = L.circle(latlon[1],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[1]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d2 = L.circle(latlon[1],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[1]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d2.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[1].tm).slice(6,8)}일 ${(asdf[1].tm).slice(8,10)}시 ${(asdf[1].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[1].ps}hPa<br>최대풍속: ${asdf[1].ws}m/s<br>강도: ${getStrength(asdf[1].ws)}<br>진행: ${asdf[1].dir}, ${asdf[1].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d2.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+                try{
+                    if(asdf[0].rad25== 0){
+                        var d1 = L.circle(latlon[0],{
+                            color: 'white',
+                            fillColor: 'rgba(255, 0, 0, 0)',
+                            radius: rad[0]*1000,
+                            dashArray: '10, 10',
+                            dashOffset: '0',
+                            weight:2
+                        }).addTo(map)
+                    }else{
+                        var d1 = L.circle(latlon[0],{
+                            color: 'red',
+                            fillOpacity:0,
+                            radius: rad[0]*1000,
+                            weight:2
+                        }).addTo(map)
+                    }
+                    d1.on('mouseover', function(e){
+                        this.bindPopup(`${(asdf[0].tm).slice(6,8)}일 ${(asdf[0].tm).slice(8,10)}시 ${(asdf[0].tm).slice(10,12)}분 예상<br>중심기압: ${asdf[0].ps}hPa<br>최대풍속: ${asdf[0].ws}m/s<br>강도: ${getStrength(asdf[0].ws)}<br>진행: ${asdf[0].dir}, ${asdf[0].sp}km/h`)
+                        this.openPopup()
+                    })
+                    d1.on('mouseout', function (e) {
+                        this.closePopup()
+                    });
+                }catch(error){
+                }
+            }
+        }
+        info_xhr.send('');
+
+
+        var now_url = 'http://apis.data.go.kr/1360000/TyphoonInfoService/getTyphoonInfo'; /*URL*/
+        var now_queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'cl5s8i4yp76CKdNIDbn0RZDOYdzjAgzPaTtbVMDqnKWHomjjBtq%2BmajQpYggkXVlfj4FY2x304%2FuTVIm1DilIw%3D%3D'; /*Service Key*/
+        now_queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /*페이지 번호 조회*/
+        now_queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('30'); /*행 수*/
+        now_queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('JSON'); /*데이터 타입 - JSON*/
         var now = new Date();
         var twoago = new Date(now.setDate(now.getDate() - 3));
         // console.log(twoago)
@@ -91,21 +361,21 @@ xhr.onreadystatechange = function () {
         // console.log(twoago_month)
         // console.log(twoago_date)
         // console.log(twoago_year)
-        queryParams += '&' + encodeURIComponent('fromTmFc') + '=' + encodeURIComponent(twoago_year + twoago_month + twoago_date); /*조회 시작 날짜 - 오늘*/
-        queryParams += '&' + encodeURIComponent('toTmFc') + '=' + encodeURIComponent(getToday()); /*조회 끝 날짜 - 오늘*/
-        xhr.open('GET', url + queryParams);
-        var api_url = url + queryParams;
-        console.log(api_url)
+        now_queryParams += '&' + encodeURIComponent('fromTmFc') + '=' + encodeURIComponent(twoago_year + twoago_month + twoago_date); /*조회 시작 날짜 - 오늘*/
+        now_queryParams += '&' + encodeURIComponent('toTmFc') + '=' + encodeURIComponent(getToday()); /*조회 끝 날짜 - 오늘*/
+        xhr.open('GET', now_url + now_queryParams);
+        var api_url = now_url + now_queryParams;
+        // console.log(api_url)
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 try {
-                    var myObj = JSON.parse(this.responseText);
+                    myObj = JSON.parse(this.responseText);
                 } catch (error) {
                     
                 }
                 myObj = myObj.response.body.items.item[0]
-                console.log(myObj)
+                // console.log(myObj)
                 // 현재 폭풍반경
                 if (myObj.typWs >= 54) {
                     strength = "초강력"
@@ -151,75 +421,19 @@ xhr.onreadystatechange = function () {
                     html: `<div class="text-label" style="width:200px">제 ${myObj.typSeq}호 태풍 '${myObj.typName}'</div>`
                 });
                 L.marker([myObj.typLat -1 , myObj.typLon +1], { icon: customIcon }).addTo(map);
-                
+
+                var now_latlon = []
+                now_latlon.push((myObj.typLat).toString())
+                now_latlon.push((myObj.typLon).toString())
+                latlon.unshift(now_latlon)
+                // console.log(latlon)
+                L.polyline(latlon, { color: 'black' }).addTo(map);
             }
         };
         
         xmlhttp.open("GET", api_url, true);
         xmlhttp.send();
         // console.log(api_url)
-
-        var info_xhr = new XMLHttpRequest();
-        var info_url = 'https://apis.data.go.kr/1360000/TyphoonInfoService/getTyphoonFcst'; /*URL*/
-        var info_queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'cl5s8i4yp76CKdNIDbn0RZDOYdzjAgzPaTtbVMDqnKWHomjjBtq%2BmajQpYggkXVlfj4FY2x304%2FuTVIm1DilIw%3D%3D'; /*Service Key*/
-        info_queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /*데이터 타입 - JSON*/
-        info_queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /*데이터 타입 - JSON*/
-        info_queryParams += '&' + encodeURIComponent('dataType') + '=' + encodeURIComponent('JSON'); /*데이터 타입 - JSON*/
-        info_queryParams += '&' + encodeURIComponent('tmFc') + '=' + encodeURIComponent(time[0]); /*조회 끝 날짜 - 오늘*/
-        info_queryParams += '&' + encodeURIComponent('typSeq') + '=' + encodeURIComponent(num[0]); /*조회 끝 날짜 - 오늘*/
-        info_xhr.open('GET', info_url + info_queryParams);
-        console.log(info_url + info_queryParams)
-        info_xhr.open('GET', info_url + info_queryParams);
-        info_xhr.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                try {
-                    var jsonData = JSON.parse(this.responseText);
-                } catch (error) {
-
-                }
-                console.log(jsonData.response.body.items.item)
-                var asdf = jsonData.response.body.items.item;
-                lat=[]
-                lat.push(asdf[0].lat)
-                lat.push(asdf[0].lon)
-                plz.push(lat)
-                for (j = 0; j < asdf.length; j++) {
-                    var latlondata = []
-                    latlondata.push(asdf[j].lat)
-                    latlondata.push(asdf[j].lon)
-                    latlon.push(latlondata)
-                }
-                console.log(latlon)
-                for (j = 0; j < latlon.length; j++) {
-                    L.marker(latlon[j], { icon: ping }).addTo(map)
-                }
-                L.polyline(latlon, { color: 'black' }).addTo(map);
-                //현재
-                if (asdf[0].ws >= 54) {
-                    strength = "초강력"
-                } else if (asdf[0].ws >= 45) {
-                    strength = "매우강"
-                } else if (asdf[0].ws >= 33) {
-                    strength = "강"
-                } else if (asdf[0].ws >= 25) {
-                    strength = "중"
-                } else {
-                    strength = "--"
-                }
-                for (j = latlon.length - 1; j >= 0; j--) {
-                    L.circle(latlon[j], {
-                        color: 'white',
-                        fillColor: 'rgba(255, 0, 0, 0)',
-                        radius: (asdf[j].radPr) * 1000,
-                        dashArray: '10, 10',
-                        dashOffset: '0',
-                        weight: 2
-                    }).addTo(map)
-                }
-
-            }
-        }
-        info_xhr.send('');
     }
 }
 xhr.send('');
